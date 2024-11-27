0635bbd26a4889fa1be89922d3ed5e7787e7e8e5
Alin RADU <alin.radu.cristian@gmail.com>
2024-11-27 20:33:02 +0200

    add intermediate operations, matching and searching

---
+++ src/section17Streams/intermediateOperations/Seat.java
@@ -0,0 +1,18 @@
+package section17Streams.intermediateOperations;
+
+public record Seat(char rowMarker, int seatNumber, double price) {
+
+    public Seat(char rowMarker, int seatNumber) {
+        this(rowMarker, seatNumber,
+                rowMarker > 'C' && (seatNumber <= 2 || seatNumber >= 9) ? 50 : 75);
+    }
+
+    public Boolean isReserved() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "%c%03d %.0f".formatted(rowMarker, seatNumber, price);
+    }
+}
