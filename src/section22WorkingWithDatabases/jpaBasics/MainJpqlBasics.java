9f54bfc29187f684617427665489f7b76ba6c0ee
Alin RADU <alin.radu.cristian@gmail.com>
2024-12-19 19:04:27 +0200

    add JPA Tuple basics

--- src/section22WorkingWithDatabases/jpaBasics/MainJpqlBasics.java
+++ src/section22WorkingWithDatabases/jpaBasics/MainJpqlBasics.java
@@ -3,14 +3,15 @@ package section22WorkingWithDatabases.jpaBasics;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Tuple;
 import section22WorkingWithDatabases.jpaBasics.music.Artist;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class MainJpqlBasics {
     public static void main(String[] args) {
 
-        List<Artist> artists = null;
         try (
                 EntityManagerFactory emf = Persistence.createEntityManagerFactory("dev.lpa.music");
                 EntityManager em = emf.createEntityManager();
@@ -19,8 +20,23 @@ public class MainJpqlBasics {
             var transaction = em.getTransaction();
             transaction.begin();
 
-            artists = getArtistsJPQL(em, "%Stev%");
+            // 1
+//            List<Artist> artists = getArtistsJPQL(em, "%Stev%");
+//            artists.forEach(System.out::println);
+
+            // 2
+//            Stream<Tuple> names = getArtistsNames(em, "%Stev%");
+//            names
+//                    .map(a -> new Artist(a.get("id", Integer.class), (String) a.get("name")))
+//                    .forEach(System.out::println);
+
+            // 3
+            List<Artist> artists = getArtistsByAlbumsJPQL(em, "%Greatest Hits%");
             artists.forEach(System.out::println);
+
+            long count = artists.size();
+
+            System.out.println("---> count: " + count);
 
             transaction.commit();
         } catch (Exception e) {
@@ -31,6 +47,24 @@ public class MainJpqlBasics {
         String jpql = "SELECT a FROM Artist a WHERE a.artistName LIKE ?1";
         var query = em.createQuery(jpql, Artist.class);
         query.setParameter(1, matchedValue);
+
+        return query.getResultList();
+    }
+
+    private static Stream<Tuple> getArtistsNames(EntityManager em, String matchedValue) {
+        String jpql = "SELECT a.artistId id, a.artistName as name FROM Artist a WHERE a.artistName LIKE ?1";
+        var query = em.createQuery(jpql, Tuple.class);
+        query.setParameter(1, matchedValue);
+
+        return query.getResultStream();
+    }
+
+    private static List<Artist> getArtistsByAlbumsJPQL(EntityManager em, String matchedValue) {
+        String jpql = "SELECT a FROM Artist a JOIN albums as album" +
+                " WHERE album.albumName LIKE ?1 OR album.albumName LIKE ?2";
+        var query = em.createQuery(jpql, Artist.class);
+        query.setParameter(1, matchedValue);
+        query.setParameter(2, "%Best of%");
 
         return query.getResultList();
     }
